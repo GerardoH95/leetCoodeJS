41199ade264132ed43acc351ab1e32d615cf91d3
Gerardo <huerta_ger@hotmail.com>
2020-05-01 19:03:46 -0500

    1 and 7 solutions

---
+++ leetCode1.js
@@ -0,0 +1,18 @@
+var twoSum = function (nums, target) {
+    var result = [];
+
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i);
+                result.push(j);
+            }
+            console.log(`i = ${i} j = ${j}`);
+        } 
+        
+    }
+    return result;
+}
+
+
+// console.log(twoSum([2, 7, 11, 15], 9));
---
+++ leetCode7.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = (num) => {
+	if(num < 0) return reverse(-1 * num) * -1;
+	const solution = parseInt(num.toString().split("").reverse().join(""));
+	if(solution > 2147483647){
+	  return 0;
+	} else{
+	  return solution;
+	}
+  }
